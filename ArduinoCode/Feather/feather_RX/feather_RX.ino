54c7c9ed088719ebcc8a008d60764212be432512
Jorge Reyes <jr31@princeton.edu>
2017-11-05 14:32:56 -0500

    made changes so that feather can transmit and recieve without having to print to Serial

--- ArduinoCode/Feather/feather_RX/feather_RX.ino
+++ ArduinoCode/Feather/feather_RX/feather_RX.ino
@@ -52,7 +52,7 @@
 */
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 915.0
+#define RF95_FREQ 868.0
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
@@ -66,11 +66,11 @@ void setup()
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
-  while (!Serial);
-  Serial.begin(9600);
-  delay(100);
+//  while (!Serial);
+//  Serial.begin(9600);
+//  delay(100);
 
-  Serial.println("Feather LoRa RX Test!");
+//  Serial.println("Feather LoRa RX Test!");
   
   // manual reset
   digitalWrite(RFM95_RST, LOW);
@@ -79,24 +79,24 @@ void setup()
   delay(10);
 
   while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
+//    Serial.println("LoRa radio init failed");
     while (1);
   }
-  Serial.println("LoRa radio init OK!");
+//  Serial.println("LoRa radio init OK!");
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
+//    Serial.println("setFrequency failed");
     while (1);
   }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+//  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, true);
+  rf95.setTxPower(23, false);
 }
 
 void loop()
@@ -106,27 +106,32 @@ void loop()
     // Should be a message for us now   
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
+
+     digitalWrite(LED, HIGH);
+     delay(1000);
+     digitalWrite(LED, LOW);
+
     
     if (rf95.recv(buf, &len))
     {
       digitalWrite(LED, HIGH);
-      RH_RF95::printBuffer("Received: ", buf, len);
-      Serial.print("Got: ");
-      Serial.println((char*)buf);
-       Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
+      //RH_RF95::printBuffer("Received: ", buf, len);
+     // Serial.print("Got: ");
+     // Serial.println((char*)buf);
+//       Serial.print("RSSI: ");
+//      Serial.println(rf95.lastRssi(), DEC);
       delay(10);
       // Send a reply
       delay(200); // may or may not be needed
       uint8_t data[] = "And hello back to you";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
-      Serial.println("Sent a reply");
+//      Serial.println("Sent a reply");
       digitalWrite(LED, LOW);
     }
     else
     {
-      Serial.println("Receive failed");
+//      Serial.println("Receive failed");
     }
   }
 }
