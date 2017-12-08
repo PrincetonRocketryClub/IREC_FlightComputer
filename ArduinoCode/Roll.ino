6acb845302ef07c1a9a15aed16f681d1618dde9a
jeffsun13 <33439170+jeffsun13@users.noreply.github.com>
2017-12-07 21:23:06 -0500

    Roll/Pitch/Heading

---
+++ ArduinoCode/Roll.ino
@@ -0,0 +1,53 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* This driver reads raw data from the BNO055
+
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
+  bno.setExtCrystalUse(true);
+}
+
+void loop(void)
+{
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+
+  /* Display the floating point data */
+ Serial.print(" PITCH: ");
+  Serial.print(euler.x());
+  Serial.print(" ROLL: ");
+  Serial.print(euler.y());
+   Serial.print(" HEADING: ");
+  Serial.print(euler.z());
+  Serial.print("\t\t");
+  Serial.println("");
+  
+
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
